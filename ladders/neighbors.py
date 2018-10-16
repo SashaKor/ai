87cb70b4e314e8f70a7f3d239e5e2cc8e14c7e0b
SashaKor <you@example.com>
2018-10-15 23:37:55 -0400

    done?

---
+++ ladders/neighbors.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python3
+
+import sys
+
+def findNeighbors(infile, outfile):
+    f = open(infile,'r').read().split('\n')
+    o = open(outfile,'w')
+    wordlst= wordList()
+
+    for word in f:
+        cnt=0 
+        if len(word) != 0:
+            for entry in wordlst:
+                if len(entry)== len(word):
+                    if len(set(word).intersection(set(entry)))== (len(word)-1):
+                        posCnt=0
+                        for i in range(0, len(entry)):
+                            if word[i]==entry[i]:
+                                posCnt+=1
+                        if posCnt == (len(word)-1):
+                            cnt+=1
+            o.write(word+","+str(cnt)+"\n")
+        
+    o.close()
+
+#open standard word list and split by line
+def wordList():
+    wordlst = open("../dictall.txt","r").read().split('\n')
+    return wordlst
+
+findNeighbors(sys.argv[1],sys.argv[2])
