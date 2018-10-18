50613a1fe63ac0f2cf8d9e6dfcbe14bdf252d3c0
SashaKor <you@example.com>
2018-10-17 20:15:18 -0400

    modification

--- ladders/neighbors.py
+++ ladders/neighbors.py
@@ -6,10 +6,12 @@ def findNeighbors(infile, outfile):
     f = open(infile,'r').read().split('\n')
     o = open(outfile,'w')
     wordlst= wordList()
+    dct= {}
 
     for word in f:
-        cnt=0 
+        cnt=0
         if len(word) != 0:
+            dct[word]=[]
             for entry in wordlst:
                 if len(entry)== len(word):
                     if len(set(word).intersection(set(entry)))== (len(word)-1):
@@ -18,14 +20,16 @@ def findNeighbors(infile, outfile):
                             if word[i]==entry[i]:
                                 posCnt+=1
                         if posCnt == (len(word)-1):
+                            dct[word].append(entry)
                             cnt+=1
             o.write(word+","+str(cnt)+"\n")
-        
+    
     o.close()
+    return dct #creation of dict as per assignment requirement   
 
 #open standard word list and split by line
 def wordList():
-    wordlst = open("../dictall.txt","r").read().split('\n')
+    wordlst = open("dictall.txt","r").read().split('\n')
     return wordlst
 
 findNeighbors(sys.argv[1],sys.argv[2])
