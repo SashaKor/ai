16cc9e1ea3e873172ecf3e9ffb59bc2dd137da28
Sasha <akoroza@stuy.edu>
2018-11-28 14:06:10 -0500

    counting

---
+++ tictac/ticCount.py
@@ -0,0 +1,10 @@
+#1) possible number of board configurations
+#2) possible number of paths
+
+def buildTree():
+    '''
+    counting number of leaves vs number of all
+    '''
+
+#if already filled
+def isSolved(board):
